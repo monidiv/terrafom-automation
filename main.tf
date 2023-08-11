4f8f71614dbb2a62df07a0fd8c1314ac3ac3bf52
root <root@ip-172-31-87-232.ec2.internal>
2023-08-11 03:06:18 +0000

    my first terraform commit

---
+++ main.tf
@@ -0,0 +1,9 @@
+provider  "aws"  {
+ region       = "us-east-1"
+ access_key   = "AKIAYE7N6BXVPN4JSNOV"
+ secret_key   = "Y9O2u7VGbS/4okVz8DEecGLHAYYoTIUprORANF1r"
+}
+
+
+resource  "aws_s3_bucket"  "example"
+bucket        = "divya1903"
